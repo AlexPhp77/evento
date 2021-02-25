3cf8ff95a1dd1cc553ac229fe787f270430c14ab
Alex R <alex.rai@hotmail.com>
2021-02-25 14:12:35 -0300

    Alterações bug

--- README.md
+++ README.md
@@ -9,12 +9,12 @@ Necessário ter um servidor local instalado e configurado.
 
 Sistema em PHP.
 
-Baixe o arquivo ZIP ou clone o diretório pelo git dentro da pasta do seu servidor LOCAL (pasta WWW)
+Baixe o arquivo ZIP ou clone o diretório pelo git dentro da pasta do seu servidor LOCAL (pasta WWW).
 
-Banco de dados MYSQL (sistema utilizado WAMP)
+Banco de dados MYSQL (sistema utilizado WAMP).
 
 Crie um banco de dados chamado: teste 
-Ou se preferir pode criar com outro nome, porém lembre-se de alterar o arquivo config.php
+Ou se preferir pode criar com outro nome, porém lembre-se de alterar o arquivo config.php.
 
-
+Após, você poderá importar o banco de dados chamado teste.sql para seu servidor LOCAL.
 
--- organizar_evento.php
+++ organizar_evento.php
@@ -5,7 +5,7 @@ $pdo->query("DELETE FROM pessoa_sala");
 
 $sql = $pdo->query("SELECT * FROM pessoas");
 
-$sql2 = $pdo->query("SELECT * FROM salas ORDER BY lotacao ASC");
+$sql2 = $pdo->query("SELECT * FROM salas ORDER BY lotacao DESC");
 
 if($sql->rowCount() <= 0 OR $sql2->rowCount() <= 0){	
 	exit;
@@ -19,15 +19,23 @@ $pessoaQt = count($pessoa);
 
 for($i=0;$i<$salas;$i++){	
     $y = 0;   
-	foreach($pessoa as $p){ 
+	foreach($pessoa as $p){ 		
 		if($y < $lotacao[$i]['lotacao']){
+
+			$sql = $pdo->prepare("SELECT pessoa_id FROM pessoa_sala WHERE pessoa_id = :pessoa_id");
+			$sql->bindValue(':pessoa_id', $p['id']);
+			$sql->execute();
+
+			if($sql->rowCount() > 0){
+				break;
+			}
+
 			$sql = $pdo->prepare("INSERT INTO pessoa_sala SET pessoa_id = :pessoa_id, sala_id = :sala_id");
 			$sql->bindValue(':pessoa_id', $p['id']);
 			$sql->bindValue(':sala_id', $lotacao[$i]['id']);
 			$sql->execute();
 
-			$y++;
-			
+			$y++;			
 	    } else {
 	        $pessoa = array_slice($pessoa, $y);
 	    	break;
